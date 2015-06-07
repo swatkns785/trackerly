e6561830cc2127744c28c95ad5f5cf68e157d493
Steven Watkins <steven.watkins78@gmail.com>
2015-06-06 20:47:47 -0400

    Configured user model tests and edited existing user feature tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name,
+    presence: true
+
+  validates :last_name,
+    presence: true
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :sign_up }
+
+  validates :password,
+    presence: true
+
+  validates :password_confirmation,
+    presence: true
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,6 +2,8 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
+    sequence(:first_name) {|n| "Bunk#{n}"}
+    sequence(:last_name) {|n| "Moreland#{n}"}
     sequence(:email) {|n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
