35fba592b713f05a5be1806377a89f7fd29a38ef
Steven Watkins <steven.watkins78@gmail.com>
2015-06-06 22:17:13 -0400

    school district model tests passing

---
+++ app/models/school_district.rb
@@ -0,0 +1,29 @@
+class SchoolDistrict < ActiveRecord::Base
+  belongs_to :user
+
+  validates :user,
+    presence: true
+
+  validates :name,
+    presence: true
+
+  validates :address,
+    presence: true
+
+  validates :city,
+    presence: true
+
+  validates :state,
+    presence: true
+
+  validates :zip_code,
+    presence: true
+
+  validates_format_of :zip_code,
+                      :with => %r{\d{5}(-\d{4})?},
+                      :message => "should be 12345 or 12345-1234"
+
+  validates :user_id,
+    presence: true,
+    numericality: { only_integer: true }
+end
