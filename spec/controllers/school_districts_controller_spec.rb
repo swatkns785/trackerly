228bc632d48fb4650ba9195aaeaf42bc8d6fbb82
Steven Watkins <steven.watkins78@gmail.com>
2015-06-06 23:44:44 -0400

    attempted controller district

--- spec/controllers/school_districts_controller_spec.rb
+++ spec/controllers/school_districts_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe SchoolDistrictsController, type: :controller do
-
+  # let(:user) { FactoryGirl.create(:user) }
+  # let(:school_district) { FactoryGirl.create(:school_district) }
+  #
+  # describe '#create' do
+  #   context 'with valid params' do
+  #     it 'redirects to show page' do
+  #       session[:user_id] = user.id
+  #       post :create, school_district: school_district
+  #
+  #       expect(SchoolDistrict.count).to eq(1)
+  #       expect(response).to redirect_to(school_district_path(SchoolDistrict.first))
+  #     end
+  #   end
+  #
+  #   context 'with invalid params' do
+  #     it 're-renders #new form'
+  #   end
+  # end
 end
